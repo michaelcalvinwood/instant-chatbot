e017fbdfd865d9135ea0d4336dc2f10823d10cfa
Michael Wood <michaelwood33311@icloud.com>
2023-04-14 12:02:21 -0400

    added Header component

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ import Login from './pages/Login';
 import Purchase from './pages/Purchase';
 import Dashboard from './pages/Dashboard';
 import ChatBot from './pages/ChatBot';
+import { Header } from './Components/Header/Header';
 
 function App() {
   return (
-   <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/login" element={<Login />} />
-    <Route path="/purchase" element={<Purchase />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/chatbot" element={<ChatBot />} />
-    
-   </Routes>
+    <div>
+     
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/purchase" element={<Purchase />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/chatbot" element={<ChatBot />} />
+          
+        </Routes>
+      
+   </div>
   );
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,79 @@
+import {
+    Avatar,
+    Box,
+    Button,
+    ButtonGroup,
+    Container,
+    Flex,
+    HStack,
+    IconButton,
+    useBreakpointValue,
+    Image
+  } from '@chakra-ui/react'
+  import { FiHelpCircle, FiMenu, FiSearch, FiSettings } from 'react-icons/fi'
+  import InstantChatBotIcon from '../../assets/images/instantChatBotoLogo.svg'
+  import { Link } from 'react-router-dom'
+  
+  export const Header = () => {
+    const isDesktop = useBreakpointValue({
+      base: false,
+      lg: true,
+    })
+    return (
+      <Box
+        as="section"
+        pb={{
+          base: '6',
+          md: '6',
+        }}
+        width="100%"
+        
+      >
+        <Box as="nav" bg="bg-accent" color="on-accent">
+          <Container
+            py={{
+              base: '3',
+              lg: '4',
+            }}
+          >
+            <Flex marginRight="64px" alignItems='center' justifyContent='center' width="100%">
+                    <img src={InstantChatBotIcon} height="64px" width="64px"/>
+                    <Box margin="none" padding="none" textAlign="center" fontWeight='700' fontSize='2rem' lineHeight='1.2'>Instant ChatBot</Box>
+                </Flex>
+            <Flex justify="space-between" >
+              <HStack spacing="4">
+                
+                {isDesktop && (
+                  <ButtonGroup variant="ghost-on-accent" spacing="1">
+                    <Link to="./"><Button>Home</Button></Link>
+                    <Link to="./dashboard"><Button>Dashboard</Button></Link>
+                    <Link to="./purchase"><Button>Purchase</Button></Link>
+                    <Link to="./login"><Button>Sign Up</Button></Link>
+                    <Link to="./login"><Button>Login</Button></Link>
+                  </ButtonGroup>
+                )}
+              </HStack>
+              {isDesktop ? (
+                <HStack spacing="4">
+                  <ButtonGroup variant="ghost-on-accent" spacing="1">
+                    <IconButton icon={<FiSearch fontSize="1.25rem" />} aria-label="Search" />
+                    <IconButton icon={<FiSettings fontSize="1.25rem" />} aria-label="Settings" />
+                    <IconButton icon={<FiHelpCircle fontSize="1.25rem" />} aria-label="Help Center" />
+                  </ButtonGroup>
+                  
+                </HStack>
+              ) : (
+                <IconButton
+                  variant="ghost-on-accent"
+                  icon={<FiMenu fontSize="1.25rem" />}
+                  aria-label="Open Menu"
+                />
+              )}
+            </Flex>
+          </Container>
+        </Box>
+       
+      </Box>
+    )
+  }
+  
