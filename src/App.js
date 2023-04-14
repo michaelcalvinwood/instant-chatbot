339e8ae24ee018444b9c01f95ea237b46e7e09ad
Michael Wood <michaelwood33311@icloud.com>
2023-04-14 10:57:34 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { Container, Heading } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Purchase from './pages/Purchase';
+import Dashboard from './pages/Dashboard';
+import ChatBot from './pages/ChatBot';
 
 function App() {
   return (
-    <Container className="App" maxWidth='4xl'>
-      <Heading>Hello Chat Bot</Heading>
-    </Container>
+   <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/login" element={<Login />} />
+    <Route path="/purchase" element={<Purchase />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/chatbot" element={<ChatBot />} />
+    
+   </Routes>
   );
 }
 
---
+++ src/pages/ChatBot.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChatBot() {
+  return (
+    <div>ChatBot</div>
+  )
+}
+
+export default ChatBot
