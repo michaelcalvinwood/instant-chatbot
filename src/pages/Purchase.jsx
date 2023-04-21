2ef7fd631da268a4069f08a9b5c4cfe90d4ba12c
Michael Wood <michaelwood33311@icloud.com>
2023-04-21 08:34:24 -0400

    added inputing monthly question quota

--- src/pages/Purchase.jsx
+++ src/pages/Purchase.jsx
@@ -1,9 +1,17 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Box, Container, Heading, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Radio, RadioGroup, Text, Stack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { RadioButton, RadioButtonGroup } from './RadioButtonGroup'
 
 function Purchase() {
   const [term, setTerm] = useState('monthly');
+  const [storageSize, setStorageSize] = useState(100);
+  const [storageUnits, setStorageUnits] = useState('MB');
+  const [storagePrice, setStoragePrice] = useState(5);
+  const [questionsPerMonth, setQuestionsPerMonth] = useState(1000);
+
+  console.log(term, storageSize);
+
+
 
   return (
     <Container>
@@ -11,7 +19,45 @@ function Purchase() {
       <RadioButtonGroup  defaultValue="left" size='md' value={term} display='block' margin="auto" width="fit-content">
             <RadioButton value="monthly" onClick={() => setTerm('monthly')}>Monthly</RadioButton>
             <RadioButton value="annual" onClick={() => setTerm('annual')}>Annual</RadioButton>
-          </RadioButtonGroup>
+      </RadioButtonGroup>
+      <Box marginTop="1rem">
+        <Text textAlign="center">Maximum Data Storage</Text>
+        <Text textAlign="center" fontSize='.75rem'>(${storagePrice} per 100MB)</Text>
+        <Box display="flex" justifyContent={"center"} alignItems={'center'} marginTop=".5rem">
+          <NumberInput width="8rem" step={1} precision={0} value={storageSize} onChange={(valueStr) => {
+            if (isNaN(valueStr)) return;
+              setStorageSize(Number(valueStr))
+            }}
+          >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+          </NumberInput>
+          <RadioGroup onChange={(e) => setStorageUnits(e)} value={storageUnits} marginLeft=".5rem">
+            <Stack direction='row'>
+              <Radio value='MB'>MB</Radio>
+              <Radio value='GB'>GB</Radio>
+            </Stack>
+          </RadioGroup>
+        </Box>
+        <Text textAlign="center" marginTop="1rem">Questions per Month</Text>
+        <Text textAlign="center" fontSize=".75rem">($12 per 1,000)</Text>
+         <Box display="flex" justifyContent={"center"}>
+          <NumberInput width="8rem" step={1000} precision={0} value={questionsPerMonth} onChange={(valueStr) => {
+                if (isNaN(valueStr)) return;
+                  setQuestionsPerMonth(Number(valueStr))
+                }}
+              >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+            </NumberInput>
+         </Box>
+      </Box>
     </Container>
   )
 }
