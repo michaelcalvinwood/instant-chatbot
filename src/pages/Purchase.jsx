9f24d15c090e22e538a33ba9cc02aa1055e2fdb0
Michael Wood <michaelwood33311@icloud.com>
2023-04-21 07:58:25 -0400

    added monthly/annual option to purchase page

--- src/pages/Purchase.jsx
+++ src/pages/Purchase.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Container, Heading } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { RadioButton, RadioButtonGroup } from './RadioButtonGroup'
 
 function Purchase() {
+  const [term, setTerm] = useState('monthly');
+
   return (
-    <div>Purchase</div>
+    <Container>
+      <Heading textAlign="center" marginBottom="1rem">Purchase</Heading>
+      <RadioButtonGroup  defaultValue="left" size='md' value={term} display='block' margin="auto" width="fit-content">
+            <RadioButton value="monthly" onClick={() => setTerm('monthly')}>Monthly</RadioButton>
+            <RadioButton value="annual" onClick={() => setTerm('annual')}>Annual</RadioButton>
+          </RadioButtonGroup>
+    </Container>
   )
 }
 
