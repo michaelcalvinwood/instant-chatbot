7cddc7776f042bfd232b0ffa3568aff8bba7f1d8
Michael Wood <michaelwood33311@icloud.com>
2023-05-02 08:52:35 -0400

    added framework shell for deleting and editing bots

--- src/Components/BotCard.jsx
+++ src/Components/BotCard.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import { Box, Heading, Image, Text } from '@chakra-ui/react'
+import React from 'react';
+import deleteIcon from '../assets/images/delete.svg';
+import editIcon from '../assets/images/edit.svg';
 
-function BotCard({bot}) {
+
+function BotCard({bot, userToken}) {
+
+  const editBot = id => {
+    console.log('editBot', id, userToken);
+  }
+
+  const deleteBot = id => {
+    console.log('deleteBot', id, userToken);
+
+  }
+
   return (
-    <div>
-        {bot.botName}
-    </div>
+    <Box className= 'bot-card' height="fit-content" padding=".5rem 1rem" margin="1rem .5rem" border="1px solid navy" borderRadius="4px" maxWidth="30%">
+        <Heading as="h6" size="xs" textAlign={"center"}>{bot.botName}</Heading>
+        <Text>{bot.websites.replaceAll("\n", ", ")}</Text>
+        <Box width="100%" display="flex" justifyContent={"space-between"} margin="0" padding="0">
+          <Image className='edit-icon' src={editIcon} height="1.25rem" width="1.25rem" onClick={() => editBot(bot.botId)}/>
+          <Image className='delete-icon' src={deleteIcon} height="1.25rem" width="1.25rem" marginLeft="-.25rem" onClick={() => deleteBot(bot.botId)}/>
+        
+        </Box>
+
+    </Box>
   )
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,16 @@ import { Box, Button, Container, Heading } from '@chakra-ui/react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import BotCard from '../Components/BotCard';
+import _ from 'lodash';
 
 function Dashboard({userName, queryTokens, storageTokens, token, hasKey}) {
   console.log('Dashboard', userName);
   const navigate = useNavigate();
   const [bots, setBots] = useState([]);
 
-
+  var arrayIsEqual = function(x, y) {
+    return _(x).differenceWith(y, _.isEqual).isEmpty();
+  };
 
   const getBots = async () => {
     const request = {
@@ -30,11 +33,11 @@ function Dashboard({userName, queryTokens, storageTokens, token, hasKey}) {
     }
 
     console.log(response.data);
-    setBots(response.data);
+    if (!arrayIsEqual(response.data, bots)) setBots(response.data);
   }
 
   useEffect(() => {
-    if (!userName) {
+    if (!userName && !bots.length) {
       console.log('navigate signup');
       navigate('/login');
     } else {
@@ -45,13 +48,14 @@ function Dashboard({userName, queryTokens, storageTokens, token, hasKey}) {
   return (
     <Container backgroundColor='white'>
       <Heading textAlign="center" marginBottom='0px'>Dashboard</Heading>
-      <Heading as='h2' size='md' noOfLines={1} textAlign={'center'} marginTop='0' marginBottom='36px' color='navy'>
-        {userName}
+      <Heading as='h2' size='md' noOfLines={1} textAlign={'center'} marginTop='0' marginBottom='18px' color='navy'>
+        Bots
       </Heading>
       <Link to='/create'><Button margin="auto" display="block">Create New Bot</Button></Link>
-      <Box>
+     
+      <Box display="flex" justifyContent={"space-between"} flexWrap="wrap" height="50vh" overflow={"auto"} padding=".5rem 1rem">
         {bots.map(bot => {
-          return <BotCard key={bot.botId} bot={bot} />
+          return <BotCard key={bot.botId} bot={bot} userToken={token}/>
         })}
       </Box>
     </Container>
