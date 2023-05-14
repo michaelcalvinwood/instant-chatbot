6172e2eb0f66ae8ca3e34911bd98acb8dd305a4b
Michael Wood <michaelwood33311@icloud.com>
2023-05-14 12:50:08 -0400

    added explanation of purchasing tokens

--- src/pages/Purchase.jsx
+++ src/pages/Purchase.jsx
@@ -26,7 +26,14 @@ function Purchase({token}) {
   return (
     <Container>
       <Heading textAlign="center" marginBottom="1rem">Purchase</Heading>
+      <Text><b>Instructions:</b> Each token costs one penny. Minimum purchase is 2,000 tokens ($20). The purhcase of tokens is non-refundable. Purchases greater than $100 receive a discount based on the amount of purchase.</Text>
+      <Text>
+          <b>Monthly Charges:</b><br/>&emsp;<b>275 tokens:</b> per Mb of storage (based upon the month's highest storage amount rounded up)<br/>
+          &emsp;<b>25 tokens:</b> per Mb of data upload (including upload data from URLs crawled rounded up)<br />
+          &emsp;<b>100 tokens:</b> per 100 queries (rounded up, minimum monthly query charge of $1)
+      </Text>
       <Button colorScheme="blue" display='block' margin='0 auto' onClick={purchaseCredits}>Buy</Button>
+     
       <Text fontSize=".75rem" fontStyle={'italic'} margin='.25rem auto' display={'block'} width="fit-content">Protected by Stripe</Text>
     </Container>
   )
